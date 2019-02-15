35f882b7784fb9040de6203f43530a7f9358be6f
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2019-02-15 08:02:22 -0200

    Resolvendo exercicio

--- concessionaria-util.js
+++ concessionaria-util.js
@@ -1,5 +1,11 @@
 function ehGolf(carro) {
-  
+  return new Promise((resolve, reject) => {
+    if(carro) {
+      resolve(carro === 'golf')
+    } else {
+      reject('Não foi informada nenhum carro!')
+    }
+  })
 }
 
 module.exports = {
