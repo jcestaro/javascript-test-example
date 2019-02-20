267d13ceecc89d8ed688eeabf43a6ef3a5b00f1f
jcestaro <joseaugusto.conceicao@bluesoft.com.br>
2019-02-20 09:45:08 -0300

    separa-lista pronto

--- separa.lista.js
+++ separa.lista.js
@@ -1,25 +1,45 @@
 function categoria(cursos, categoria) {
-  
+  return cursos.filter((cursos) => {
+    if(cursos.categoria == categoria){
+      return cursos
+    }
+  })
 }
 
 function duracaoMaiorQue(cursos, duracao) {
-  
+  return cursos.filter((cursos) => {
+    if(cursos.duracao > duracao){
+      return cursos
+    }
+  })
 }
 
 function duracaoMenorQue(cursos, duracao) {
-  
+  return cursos.filter((cursos) => {
+    if(cursos.duracao < duracao){
+      return cursos
+    }
+  })
 }
 
 function comProfessor(cursos) {
-  
+  return cursos.filter((cursos) => {
+    if(cursos.professor){
+      return cursos
+    }
+  })
 }
 
 function semProfessor(cursos) {
-  
+  return cursos.filter((cursos) => {
+    if(!cursos.professor){
+      return cursos
+    }
+  })
 }
 
 function descricao(cursos, regex) {
-  
+  return cursos.filter((cursos) => cursos.descricao.match(regex))
 }
 
 module.exports = {
